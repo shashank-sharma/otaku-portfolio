b55b3f150626bcfe8f81d5498fde7605649852f1
Shashank Sharma <shashank.sharma98@gmail.com>
2020-10-19 18:07:58 +0530

    Updates

---
+++ precache-manifest.36c90c5de65d2d6b66407e9faaf6a2cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fea836de1279ddf139039d61c0f22c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e25f166c1ca35765ed7f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "851a9f5e87b620669d8a",
+    "url": "/static/js/2.c5d15a24.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c5d15a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e25f166c1ca35765ed7f",
+    "url": "/static/js/main.61e6ac5d.chunk.js"
+  },
+  {
+    "revision": "a7a2f7fff5b63c3fb39f",
+    "url": "/static/js/runtime-main.3c1f03ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
